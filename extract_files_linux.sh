333564e84598edcf07f2e67449265d1ac6f67b39
Anthony <anthony.onwuli16@imperial.ac.uk>
2022-08-10 16:25:24 +0100

    tested new extract_files bash scripts on RHEL/mac

---
+++ extract_files_linux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# untar all the phonon data
+mkdir phonon_db
+FILES="phonon_db_tarred/*.lzma"
+
+for f in $FILES
+do
+    echo "Processing $f file"
+    tar -xvpf $f --directory phonon_db
+done
